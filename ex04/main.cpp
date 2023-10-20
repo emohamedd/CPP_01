c1cec262e5802f378e0a0990f116f664b6596b0f
emohamedd <a.elgaouej@gmail.com>
2023-10-20 15:03:11 +0100

    finishing the ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,17 +6,20 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 12:21:46 by emohamed          #+#    #+#             */
-/*   Updated: 2023/10/20 13:38:13 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/10/20 15:02:30 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
+#include <iostream>
+#include <string>
+#include <fstream>
 std::string charToString(char* str) {
     if (str) {
         return std::string(str);
     } else {
-        return std::string(); // Return an empty string if the char* is null
+        return std::string();
     }
 }
 
@@ -31,21 +34,35 @@ int main(int ac, char **av) {
     std::string replace = charToString(av[3]);
 
     if (inputFile.is_open()) {
+        std::ofstream outputFile("replaced.txt");
+
+        if (!outputFile.is_open()) {
+            std::cerr << "Failed to create the output file." << std::endl;
+            inputFile.close();
+            return 1;
+        }
+
         std::string line;
 
         while (std::getline(inputFile, line)) {
-            size_t startPos = 0;
-            while ((startPos = line.find(name)) != std::string::npos) {
-                line.replace(startPos, name.length(), replace);
-                startPos += replace.length();
+            size_t start = 0;
+            size_t end;
+            while ((end = line.find(name, start)) != std::string::npos) {
+                outputFile << line.substr(start, end - start);
+                outputFile << replace;
+                start = end + name.length();
             }
-
-            std::cout << line << std::endl;
+            if (line.find(name, start) == std::string::npos) {
+                std::cout << "Word Not found" << std::endl;
+                return 1;
+            }
+            outputFile << line.substr(start) << std::endl;
         }
 
         inputFile.close();
+        outputFile.close();
     } else {
-        std::cerr << "Failed to open the input file." << std::endl;
+        std::cerr << "Failed to open file." << std::endl;
         return 1;
     }
 
